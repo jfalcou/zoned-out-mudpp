ffd0c0f32345a5fc4c89b3a40d5c19a97a98f57b
Joel Falcou <joel.falcou@lri.fr>
2019-12-31 22:58:11 +0100

    Add timestamp iomanip

---
+++ include/mudpp/system/timestamp.hpp
@@ -0,0 +1,30 @@
+//==================================================================================================
+/**
+  MudPP - MUD engine for C++
+  Copyright 2019 Joel FALCOU
+
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#ifndef MUDPP_SYSTEM_TIMESTAMP_HPP_INCLUDED
+#define MUDPP_SYSTEM_TIMESTAMP_HPP_INCLUDED
+
+#include <iostream>
+#include <iomanip>
+#include <chrono>
+
+namespace mudpp
+{
+  struct timestamp_ {};
+  constexpr timestamp_ timestamp = {};
+
+  inline std::ostream& operator<<(std::ostream& os, timestamp_ const&)
+  {
+    std::time_t now_c = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    os << "[" << std::put_time(std::localtime(&now_c), "%F %T") << "]";
+    return os;
+  }
+}
+
+#endif
